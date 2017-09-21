3f45f6f86ffef26d4936a503f02e094a8306d9e8
nick <leakl@qq.com>
2017-09-21 10:45:22 +0800

    igoner

--- ui/config/index.js
+++ ui/config/index.js
@@ -28,8 +28,8 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-        "/admin/**":"http://192.168.8.181:812/",
-        "/uploads/**":"http://192.168.8.181:812/"
+        "/admin/**":"http://192.168.8.87:91/",
+        "/uploads/**":"http://192.168.8.87:91/"
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
