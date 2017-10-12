385a7f91c806fe13712d57e3d99ff62ee0a9956c
nameijyy <804104532@qq.com>
2017-10-12 14:16:37 +0800

    足迹

--- ui/src/mini/components/favorite.vue
+++ ui/src/mini/components/favorite.vue
@@ -7,8 +7,8 @@
                     <div class="title">{{item.good_title}}</div>
                     <div class="tool">
                         <span class="price">￥
-                                <em>{{item.price}}</em>
-                            </span>
+                            <em>{{item.price}}</em>
+                        </span>
                     </div>
                 </div>
                 <i class="iconfont icon-shanchu " @click="ondel(idx);"></i>
@@ -22,65 +22,71 @@
     </div>
 </template>
 <script>
-    import http from '@/assets/js/http'
-    export default {
-        name: 'favorite',
-        mixins: [http],
-        data() {
-            return {
-                list: [],
-                pages: {},
-                timer: null
-            }
-        },
-        methods: {
-            loadMore() {
-                let page = parseInt(this.pages.current_page, 10);
-                clearTimeout(this.timer)
-                this.timer = setTimeout(() => {
-                    if (page < this.pages.total_page) {
-                        this.get_list(page + 1);
-                    }
-                }, 500)
-
-
-            },
-            get_list(page) {
-                page = page || 1;       
-                let url = '/mini/Collect/get_list?page='+page,
-                    vm = this;
-                this.apiGet(url, {}).then(function(res) {
-                    if (res.code) {
-                        vm.pages = res.data.pages;
-                        if (page < 2) {
-                            vm.list = res.data.list;
-                        } else {
-                            let _list = vm.list;
-                            _list = _list.concat(res.data.list)
-                            vm.list = _list;
-                        }
-                    } else {
-                        vm.handleError(res)
-                    }
-                 
-                })
-            },
-            //删除
-            ondel(index) {
-                let url = '/mini/Collect/del', vm = this, data = { good_id: this.list[index].good_id };
-                this.apiPost(url, data).then(function(res) {
-                    if (res.code) {
-                        vm.$msg(res.msg);
-                        vm.list.splice(index,1);
-                    } else {
-                        vm.handleError(res)
-                    }
-                })
-            },
-        },
-        created() {
-            this.setTitle('我的收藏')
-            this.get_list();
+import http from '@/assets/js/http'
+export default {
+    name: 'favorite',
+    mixins: [http],
+    data() {
+        return {
+            list: [],
+            pages: {},
+            timer: null
         }
+    },
+    methods: {
+        loadMore() {
+            let page = parseInt(this.pages.current_page, 10);
+            clearTimeout(this.timer)
+            this.timer = setTimeout(() => {
+                if (page < this.pages.total_page) {
+                    this.get_list(page + 1);
+                }
+            }, 500)
+
+
+        },
+        get_list(page) {
+            page = page || 1;
+            let url = '/mini/Collect/get_list?page=' + page,
+                vm = this;
+            this.apiGet(url, {}).then(function(res) {
+                if (res.code) {
+                    vm.pages = res.data.pages;
+                    if (page < 2) {
+                        vm.list = res.data.list;
+                    } else {
+                        let _list = vm.list;
+                        _list = _list.concat(res.data.list)
+                        vm.list = _list;
+                    }
+                } else {
+                    vm.handleError(res)
+                }
+
+            })
+        },
+        //删除
+        ondel(index) {
+            let url = '/mini/Collect/del', vm = this, data = { good_id: this.list[index].good_id };
+            this.$confirm({
+                msg: '确定删除此收藏？',
+                yes: function() {
+                    vm.apiPost(url, data).then(function(res) {
+                        if (res.code) {
+                            vm.$msg(res.msg);
+                            vm.list.splice(index, 1);
+                        } else {
+                            vm.handleError(res)
+                        }
+                    })
+                }
+            })
+
+        },
+    },
+    created() {
+        this.setTitle('我的收藏')
+        this.get_list();
     }
+}
 </script>
