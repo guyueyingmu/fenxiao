046090d633960e2a8208f6ca9aac06d2230a880a
nick <leakl@qq.com>
2017-10-17 12:24:26 +0800

    1222

--- ui/src/mini/vuex/state.js
+++ ui/src/mini/vuex/state.js
@@ -1,15 +1,6 @@
 const state ={
     DEV : window.location.port == '8080',
     ShowNav:true,
-    search:{
-        keyword:'',
-        loading:false,
-        sort:true,
-        cat_idx:0,
-        
-    },
-    list:[],
-    hList:[],
     title:'首页',
     cart:[],
     checked_address:{},
