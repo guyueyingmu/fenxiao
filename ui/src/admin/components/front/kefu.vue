60aad3cfdc83c99bcecaf43e4386dd1f8e267574
nick <leakl@qq.com>
2017-10-24 16:42:59 +0800

    1213

--- ui/src/admin/components/front/kefu.vue
+++ ui/src/admin/components/front/kefu.vue
@@ -37,8 +37,10 @@
 
         <div class="reply_list_main">
             <div v-show="reply_show" class="reply_list_dialog_bg" @click="close_reply"></div>
+         
             <transition name="left">
                 <div v-show="reply_show" class="reply_list_dialog">
+                       <div  class="loadmore-msg" v-show="loadmore_f">加载更多信息...</div>
                     <div class="reply_list_box">
                         <div class="reply_list_content" id="reply_list_content">
                             <div class="sd-scroller" v-loading="replyLoading">
@@ -97,244 +99,274 @@
     </div>
 </template>
 <script>
-import http from '@/assets/js/http'
-import Iscroll from '@/assets/js/iscroll'
+import http from "@/assets/js/http";
+import Iscroll from "@/assets/js/iscroll";
 export default {
-    mixins: [http],
+  mixins: [http],
 
-    data() {
-        return {
-            d_zoon: false,
-            d_z_url: '',
-            isSearch: false,
-            replyLoading: false,
-            formInline: {
-                keyword: '',
-
-            },
-            dialog: {
-                info: {
-                    content: '',
-                    user_id: '',
-                },
-                list: []
-
-            },
-            list: [],
-            reply_show: false,
-            current_user_id: 0,
-            current_client_id: '',
+  data() {
+    return {
+      d_zoon: false,
+      d_z_url: "",
+      isSearch: false,
+      replyLoading: false,
+      loadmore_f: false,
+      formInline: {
+        keyword: ""
+      },
+      dialog: {
+        info: {
+          content: "",
+          user_id: ""
+        },
+        list: []
+      },
+      list: [],
+      reply_show: false,
+      current_user_id: 0,
+      current_client_id: "",
+      r_pages: {}
+    };
+  },
+  methods: {
+    //回复
+    open_replyDialog(item) {
+      this.dialog.list = [];
+      this.dialog.info = JSON.parse(JSON.stringify(item));
+      this.dialog.info.content = "";
+      this.reply_show = true;
+      this.replyLoading = true;
+      document.body.style.overflow = "hidden";
+      this.bind_ws(item.user_id);
+      this.current_user_id = item.user_id;
+      this.get_talk_list(item.message_group_id);
+    },
+    bind_ws(user_id) {
+      // const ip = window.location.hostname;
+      const ip = "mall.minbbo.com";
+      const ws = new WebSocket("ws://" + ip + ":8282");
+      let vm = this;
+      ws.onmessage = function(e) {
+        // json数据转换成js对象
+        var data = eval("(" + e.data + ")");
+        var type = data.type || "";
+        switch (type) {
+          case "init":
+            let url = "/admin/Kefu/bind";
+            vm.current_client_id = data.client_id;
+            vm
+              .apiPost(url, { client_id: data.client_id, user_id: user_id })
+              .then(res => {
+                console.log(res);
+                if (res.code) {
+                  // vm.serverInit = false
+                }
+              });
+            break;
+          case "msg":
+            console.log(1, data.content);
+            vm.dialog.list.push(data.content);
+            setTimeout(() => {
+              IScroll1.refresh();
+            }, 200);
+            setTimeout(() => {
+              IScroll1.scrollTo(0, IScroll1.maxScrollY, 200);
+            }, 300);
+            break;
+          // 当mvc框架调用GatewayClient发消息时直接alert出来
+          default:
+          //                alert(e.data);
         }
+      };
     },
-    methods: {
-        //回复
-        open_replyDialog(item) {
-            this.dialog.list = [];
-            this.dialog.info = JSON.parse(JSON.stringify(item));
-            this.dialog.info.content = '';
-            this.reply_show = true;
-            this.replyLoading = true;
-            document.body.style.overflow = 'hidden';
-            this.bind_ws(item.user_id);
-            this.current_user_id = item.user_id;
-            this.get_talk_list(item.message_group_id);
-        },
-        bind_ws(user_id){
-            const ip = window.location.hostname;
-            const  ws = new WebSocket("ws://"+ ip +":8282");
-            let vm = this;
-            ws.onmessage = function(e) {
-                // json数据转换成js对象
-                var data = eval("(" + e.data + ")");
-                var type = data.type || '';
-                switch (type) {
-                    // Events.php中返回的init类型的消息，将client_id发给后台进行uid绑定
-                    case 'init':
-                        // console.log(data);
-                        // 利用jquery发起ajax请求，将client_id发给后端进行uid绑定
-                        //                $.post('./bind.php', {client_id: data.client_id}, function(data){}, 'json');
-                        let url = '/admin/Kefu/bind';
-                        vm.current_client_id = data.client_id;
-                        vm.apiPost(url,{client_id: data.client_id, user_id: user_id}).then(res=>{
-                            console.log(res)
-                            if(res.code){
-                                // vm.serverInit = false
-                            }
-                        })
-                        break;
-                    case 'msg': console.log(1,data.content);
-                        vm.dialog.list.push(data.content);
-                        setTimeout(() => {
-                            IScroll1.refresh();
-                        }, 200)
-                        setTimeout(() => {
-                            IScroll1.scrollTo(0, IScroll1.maxScrollY, 200)
-                        }, 300)
-                        break;
-                    // 当mvc框架调用GatewayClient发消息时直接alert出来
-                    default:
-                    //                alert(e.data);
-                }
-            };
-        },
-        close_reply() {
-            this.reply_show = false;
-            document.body.style.overflow = 'auto'
-            IScroll1.disable();
+    close_reply() {
+      this.reply_show = false;
+      document.body.style.overflow = "auto";
+      IScroll1.disable();
 
-            let user_id = this.current_user_id;
-            this.current_user_id = 0;
-            //退出聊天分组
-            let url = '/admin/Kefu/leave_group'
-            this.apiPost(url,{client_id: this.current_client_id, user_id: user_id}).then(res=>{
-                console.log(res)
-            })
-        },
-        //小图上传成功
-        messageImgSuccess(res, file) {
-            if (res.code) {
-                let _d = {};
-                _d.message_group_id = this.dialog.info.message_group_id
-                _d.type = 2;
-                _d.content = res.data.img_path
-                this.send(_d)
-            }
-        },
-        //小图上传前处理
-        beforeAvatarUpload(file) {
-            const isJPG = file.type == 'image/jpeg';
-            const isPNG = file.type == 'image/png';
-            let isTypeOk = false;
-            const isLt2M = file.size / 1024 / 1024 < 2;
-            if (!isLt2M) {
-                this.$message.error('上传图片大小不能超过 2MB!');
-            }
-
-            if (!isPNG && !isJPG) {
-                this.$message.error('上传图片只能是 PNG 或 JPG 格式!');
-
-            } else {
-                isTypeOk = true
-            }
-            return isLt2M && isTypeOk;
-        },
-        //取对话数据
-        get_talk_list(message_group_id) {
-            let url = '/admin/Kefu/detail?message_group_id=' + message_group_id, vm = this;
-            this.apiGet(url).then((res) => {
-                if (res.code) {
-                    vm.dialog.list = res.data.list;
-                    this.replyLoading = false;
-                    setTimeout(() => {
-                        IScroll1.refresh();
-                    }, 600)
-                    setTimeout(() => {
-                        IScroll1.scrollTo(0, IScroll1.maxScrollY, 200)
-                        IScroll1.enable();
-                    }, 700)
-
-
-                } else {
-                    vm.handleError(res)
-                }
-            })
-        },
-        onZoom(url) {
-            this.d_zoon = true
-            this.d_z_url = url
-
-        },
-        onSend() {
-            let _d = {};
-            _d.message_group_id = this.dialog.info.message_group_id
-            _d.type = 1;
-            _d.content = this.dialog.info.content;
-            this.send(_d)
-            this.dialog.info.content = ''
-        },
-        //取对话数据
-        send(data) {
-            let url = '/admin/Kefu/add', vm = this;
-            let _d = data;
-         
-
-            this.apiPost(url, _d).then((res) => {
-                if (res.code) {
-                    this.$msg(res.msg);
-                } else {
-                    vm.handleError(res)
-                }
-            })
-        },
-        //清空
-        onReset() {
-            this.formInline = {
-                keyword: '',
-            }
-            this.get_list(1)
-            this.isSearch = false;
-        },
-        //搜索
-        onSearch(current_paged) {
-
-            this.isSearch = true;
-            current_paged = current_paged || 1;
-            let searchData = this.formInline
-            this.get_list(current_paged, searchData)
-        },
-
-        //取数据
-        get_list(page, searchData) {
-            page = page || 1;
-            let url = '/admin/kefu/get_list?page=' + page,
-                vm = this;
-            vm.loading = true;
-            this.apiGet(url, searchData).then(function(res) {
-                if (res.code) {
-                    vm.list = res.data.list;
-                    if (vm.isSearch == false) {
-                        //通知全局商品分类数据
-                        vm.setCatList(res.data.list)
-
-                    }
-                    vm.pages = res.data.pages
-                } else {
-                    vm.handleError(res)
-                }
-                vm.loading = false;
-            })
-        },
-
-
-
+      let user_id = this.current_user_id;
+      this.current_user_id = 0;
+      //退出聊天分组
+      let url = "/admin/Kefu/leave_group";
+      this.apiPost(url, {
+        client_id: this.current_client_id,
+        user_id: user_id
+      }).then(res => {
+        console.log(res);
+      });
     },
-    //组件初始化
-    created() {
-        this.get_list();
-        this.setBreadcrumb(['前台', '在线客服'])
+    //小图上传成功
+    messageImgSuccess(res, file) {
+      if (res.code) {
+        let _d = {};
+        _d.message_group_id = this.dialog.info.message_group_id;
+        _d.type = 2;
+        _d.content = res.data.img_path;
+        this.send(_d);
+      }
+    },
+    //小图上传前处理
+    beforeAvatarUpload(file) {
+      const isJPG = file.type == "image/jpeg";
+      const isPNG = file.type == "image/png";
+      let isTypeOk = false;
+      const isLt2M = file.size / 1024 / 1024 < 2;
+      if (!isLt2M) {
+        this.$message.error("上传图片大小不能超过 2MB!");
+      }
+
+      if (!isPNG && !isJPG) {
+        this.$message.error("上传图片只能是 PNG 或 JPG 格式!");
+      } else {
+        isTypeOk = true;
+      }
+      return isLt2M && isTypeOk;
+    },
+    //取对话数据
+    get_talk_list(message_group_id, page) {
+      page = page || 1;
+      let url =
+          "/admin/Kefu/detail?message_group_id=" +
+          message_group_id +
+          "&page=" +
+          page,
+        vm = this;
+      this.apiGet(url).then(res => {
+        if (res.code) {
+          vm.r_pages = res.data.pages;
+
+          if (res.data.pages.current_page < 2) {
+            vm.dialog.list = res.data.list;
+            setTimeout(() => {
+              IScroll1.refresh();
+            }, 600);
+            setTimeout(() => {
+              IScroll1.scrollTo(0, IScroll1.maxScrollY, 200);
+              IScroll1.enable();
+            }, 750);
+          } else {
+            let _list = vm.dialog.list;
+            vm.dialog.list = res.data.list.concat(_list);
+            vm.loadmore_f = false;
+          }
+          this.replyLoading = false;
+        } else {
+          vm.handleError(res);
+        }
+      });
+    },
+    onZoom(url) {
+      this.d_zoon = true;
+      this.d_z_url = url;
+    },
+    onSend() {
+      let _d = {};
+      _d.message_group_id = this.dialog.info.message_group_id;
+      _d.type = 1;
+      _d.content = this.dialog.info.content;
+      this.send(_d);
+      this.dialog.info.content = "";
+    },
+    //取对话数据
+    send(data) {
+      let url = "/admin/Kefu/add",
+        vm = this;
+      let _d = data;
+
+      this.apiPost(url, _d).then(res => {
+        if (res.code) {
+          // vm.$msg(res.msg);
+        } else {
+          vm.handleError(res);
+        }
+      });
+    },
+    //清空
+    onReset() {
+      this.formInline = {
+        keyword: ""
+      };
+      this.get_list(1);
+      this.isSearch = false;
+    },
+    //搜索
+    onSearch(current_paged) {
+      this.isSearch = true;
+      current_paged = current_paged || 1;
+      let searchData = this.formInline;
+      this.get_list(current_paged, searchData);
+    },
+
+    //取数据
+    get_list(page, searchData) {
+      page = page || 1;
+      let url = "/admin/kefu/get_list?page=" + page,
+        vm = this;
+      vm.loading = true;
+      this.apiGet(url, searchData).then(function(res) {
+        if (res.code) {
+          vm.list = res.data.list;
+          if (vm.isSearch == false) {
+            //通知全局商品分类数据
+            vm.setCatList(res.data.list);
+          }
+          vm.pages = res.data.pages;
+        } else {
+          vm.handleError(res);
+        }
+        vm.loading = false;
+      });
+    },
+    loadmore() {
+      console.log("load more");
+      let message_group_id = this.dialog.info.message_group_id;
+      if (window.IScroll1.maxScrollY < 0 && window.IScroll1.y == 0) {
+        let page = parseInt(this.r_pages.current_page, 10);
+        if (page < parseInt(this.r_pages.total_page, 10)) {
+          page = page + 1;
+          this.loadmore_f = true;
+          this.get_talk_list(message_group_id, page);
+        }
+      }
+    }
+  },
+  //组件初始化
+  created() {
+    let vm = this;
+    this.get_list();
+    this.setBreadcrumb(["前台", "在线客服"]);
+    setTimeout(() => {
+      if (window.IScroll1 == null || !window.IScroll1) {
+        window.IScroll1 = new IScroll("#reply_list_content", {
+          mouseWheel: true,
+          scrollbars: true
+        });
+
+        window.IScroll1.on("scrollEnd", function() {
+          vm.loadmore();
+        });
+      }
+    }, 500);
+  },
+  beforeRouteEnter(to, from, next) {
+    next(vm => {
+      if (window.IScroll1 == null || !window.IScroll1) {
         setTimeout(() => {
-            window.IScroll1 = new IScroll('#reply_list_content', { mouseWheel: true, scrollbars: true });
-        }, 500)
-
-    }, beforeRouteEnter(to, from, next) {
-        next(vm => {
-            if (window.IScroll1 == null || !window.IScroll1) {
-                setTimeout(() => {
-                    window.IScroll1 = new IScroll('#reply_list_content', { mouseWheel: true, scrollbars: true });
-                }, 500)
-            }
-
-        })
-
-    }
-    , beforeRouteLeave(to, from, next) {
-        if (window.IScroll1) {
-            window.IScroll1.destroy();
-        }
-
-        window.IScroll1 = null;
-        next()
+          window.IScroll1 = new IScroll("#reply_list_content", {
+            mouseWheel: true,
+            scrollbars: true
+          });
+        }, 500);
+      }
+    });
+  },
+  beforeRouteLeave(to, from, next) {
+    if (window.IScroll1) {
+      window.IScroll1.destroy();
     }
 
-}
+    window.IScroll1 = null;
+    next();
+  }
+};
 </script>
