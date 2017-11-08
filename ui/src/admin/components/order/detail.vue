ed8a82547736b0e471766382c98f958ae233083b
nick <leakl@qq.com>
2017-11-08 16:13:44 +0800

    BUG

--- ui/src/admin/components/order/detail.vue
+++ ui/src/admin/components/order/detail.vue
@@ -5,52 +5,66 @@
             <el-row class="order_info_main_grid" :gutter="50">
                 <el-col :span="12">
                     <h4 class="pen_title">订单信息</h4>
-                    订单编号：
-                    <span>{{order_info.order_number}}</span> <br> 下单用户：
-                    <span>{{order_info.nickname}}</span> <br> 用户手机：
-                    <span>{{order_info.phone_number}}</span> <br> 下单时间：
-                    <span>{{order_info.add_time}}</span> <br> 订单总额：
-                    <span>{{order_info.total_amount}}</span> <br> 积分扣减：
-                    <span>{{order_info.minus_credits}}</span> <br> 订单状态：
-                    <span>{{order_info.order_status_txt}}</span> <br> 订单来源：
-                    <span>{{order_info.order_from_txt}}</span> <br> 订单取消原因：
-                    <span>{{order_info.cancel_reason_txt}}</span> <br> 订单分成处理：
-                    <span>{{order_info.distribution_status_txt}}</span> <br> 订单取消时间：
-                    <span>{{order_info.cancel_time}}</span> <br> 订单完成时间：
-                    <span>{{order_info.finish_time}}</span> <br>
+                    <div>订单编号：<span>{{order_info.order_number}}</span></div> 
+                   <div> 下单时间：<span>{{order_info.add_time}}</span></div> 
+                    <div>下单用户：<span>{{order_info.nickname}}</span> </div> 
+                    <div>用户手机：<span>{{order_info.phone_number}}</span></div> 
+                    <hr>
+                    <div>订单总额：<span>{{order_info.total_amount}}</span> </div> 
+                    <div>订单状态：<span>{{order_info.order_status_txt}}</span></div>
+                    <div> 订单来源：<span>{{order_info.order_from_txt}}</span> </div>
+                    <hr>
+                    <div>积分扣减：<span>{{order_info.minus_credits}}</span> </div>
+                    <div>订单分成处理：<span>{{order_info.distribution_status_txt}}</span></div>
+                    <hr>
+                    <div>订单取消原因：<span>{{order_info.cancel_reason_txt}}</span></div>
+                    <div> 订单取消时间：<span>{{order_info.cancel_time}}</span></div>
+                    <hr>
+                    <div> 订单完成时间：<span>{{order_info.finish_time}}</span> </div> 
                 </el-col>
                 <el-col :span="12">
                     <h4 class="pen_title">支付信息</h4>
-                    支付状态：
-                    <span>{{order_info.pay_status_txt}}</span> <br> 支付时间：
-                    <span>{{order_info.pay_time}}</span> <br> 退款时间：
-                    <span>{{order_info.refund_time}}</span> <br> 支付交易号：
-                    <span>{{order_info.pay_trade_num}}</span> <br> 退款交易号：
-                    <span>{{order_info.refund_trade_num}}</span> <br> 支付备注：
-                    <span>{{order_info.pay_note}}</span> <br> 退款备注：
-                    <span>{{order_info.refund_none}}</span>
-                </el-col>
+                   <div>支付状态：<span>{{order_info.pay_status_txt}}</span> </div>
+                   <div> 支付时间： <span>{{order_info.pay_time}}</span> </div>
+                   <div> 支付交易号： <span>{{order_info.pay_trade_num}}</span> </div>
+                   <div> 支付备注： <span>{{order_info.pay_note}}</span> </div>
+                   <hr>
+                   <div> 退款时间： <span>{{order_info.refund_time}}</span></div>
+                   <div> 退款交易号： <span>{{order_info.refund_trade_num}}</span> </div>
+                   <div> 退款备注： <span>{{order_info.refund_none}}</span></div>
+                   </el-col>
                 </el-col>
             </el-row>
 
             <el-row class="order_info_main_grid" :gutter="50">
                 <el-col :span="12">
                     <h4 class="pen_title">配送信息</h4>
-                    发货员：
-                    <span>{{order_info.order_consignment.consignment_user}}</span> <br> 发货时间：
-                    <span>{{order_info.order_consignment.consignment_time}}</span> <br> 配送方式：
-                    <span>{{order_info.order_consignment.deliver_method_txt}}</span> <br> 快递编号：
-                    <span>{{order_info.order_consignment.tracking_number}}</span> <br> 收货人：
-                    <span>{{order_info.consignee_name}}</span><br> 收货人电话：
-                    <span>{{order_info.consignee_phone}}</span> <br> 收货人地址：
-                    <span>{{order_info.consignee_address}}</span>
+                    <div v-if="order_info.order_consignment"> 
+                        <div>发货员：<span>{{order_info.order_consignment.consignment_user}}</span></div> 
+                        <div>发货时间：<span>{{order_info.order_consignment.consignment_time}}</span> </div> 
+                        <div>配送方式：<span>{{order_info.order_consignment.deliver_method_txt}}</span> </div> 
+                        <div>快递编号：<span>{{order_info.order_consignment.tracking_number}}</span> </div> 
+                        <hr>
+                        <div>收货人：<span>{{order_info.consignee_name}}</span></div> 
+                        <div>收货人电话：<span>{{order_info.consignee_phone}}</span></div> 
+                        <div>收货人地址： <span>{{order_info.consignee_address}}</span></div> 
+                    </div>
+                  <div v-else>
+                      <div style="color:#888;line-height:4;margin-bottom:50px">没有配送信息</div>
+                  </div>
+
                 </el-col>
                 <el-col :span="12">
                     <h4 class="pen_title">服务信息</h4>
-                    服务员：
-                    <span>{{order_info.service_user}}</span> <br> 服务时间：
-                    <span>{{order_info.service_time}}</span> <br> 备注：
-                    <span>{{order_info.note}}</span>
+                    <div v-if="order_info.order_service">
+                        <div>服务员：<span>{{order_info.order_service.service_user}}</span></div> 
+                        <div>服务时间：<span>{{order_info.order_service.service_time}}</span> </div> 
+                        <div>备注：<span>{{order_info.order_service.note}}</span></div> 
+                    </div>
+                       <div v-else>
+                      <div style="color:#888;line-height:4;margin-bottom:50px">没有服务信息</div>
+                  </div>
+              
                 </el-col>
                 </el-col>
             </el-row>
@@ -69,6 +83,7 @@
                 <el-table-column prop="price" label="购买单价" width="120"> </el-table-column>
                 <el-table-column prop="buy_num" label="购买数量"></el-table-column>
             </el-table>
+            <div style="height:180px;"></div>
         </div>
 
         <div class="table_nodata" v-else>
@@ -78,47 +93,43 @@
 </template>
 
 <script>
-import http from '@/assets/js/http'
+import http from "@/assets/js/http";
 
 export default {
-    mixins: [http],
-    data() {
-        return {
-            order_info: {}
+  mixins: [http],
+  data() {
+    return {
+      order_info: {}
+    };
+  },
+  watch: {
+    $route: "initData"
+  },
+  methods: {
+    initData() {
+      this.order_info = {};
+      this.get_order_info(this.$route.params.order_id);
+    },
+
+    //取数据
+    get_order_info(order_id) {
+      let url = "/admin/order/get_order_info?order_id=" + order_id,
+        vm = this;
+      vm.loading = true;
+      this.apiGet(url).then(function(res) {
+        if (res.code) {
+          vm.order_info = res.data;
+        } else {
+          vm.handleError(res);
         }
-    },
-    watch: {
-        $route: "initData"
-    },
-    methods: {
-        initData() {
-            this.order_info = {};
-            this.get_order_info(this.$route.params.order_id);
-        },
-
-        //取数据
-        get_order_info(order_id) {
-            let url = '/admin/order/get_order_info?order_id=' + order_id,
-                vm = this;
-            vm.loading = true;
-            this.apiGet(url).then(function(res) {
-                if (res.code) {
-                    vm.order_info = res.data;
-                } else {
-                    vm.handleError(res)
-                }
-                vm.loading = false;
-            })
-        },
-
-    },
-    //组件初始化
-    created() {
-
-        this.get_order_info(this.$route.params.order_id);
-        this.setBreadcrumb(['订单', '订单详情'])
-        
+        vm.loading = false;
+      });
     }
-
-}
+  },
+  //组件初始化
+  created() {
+    this.get_order_info(this.$route.params.order_id);
+    this.setBreadcrumb(["订单", "订单详情"]);
+  }
+};
 </script>
